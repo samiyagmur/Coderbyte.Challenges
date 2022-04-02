1b01ee434ee7099795a04b8f3ad96bd8eb147514
samiyagmur <samiyagmur92@gmail.com>
2022-04-02 16:28:56 +0300

    algorithm my works

---
+++ Codeland.Username.Validation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Codeland.Username.Validation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CodelandUsernameValidation(Console.ReadLine()));
+        }
+        public static bool CodelandUsernameValidation(string str)
+        {   
+           
+                Regex regex = new Regex(@"^([a-zA-Z])([a-zA-Z0-9_]{2,23})([^_])$");
+
+                Match match = regex.Match(str);
+
+                return match.Success;
+  
+                
+
+        }
+
+    }
+}
+//1.The username is between 4 and 25 characters.
+//2.It must start with a letter.
+//3. It can only contain letters, numbers, and the underscore character.
+//4. It cannot end with an underscore character.
---
+++ Questions.Marks/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Questions.Marks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(QuestionsMarks(Console.ReadLine()));
+        }
+        public static string QuestionsMarks(string str)
+        {   
+             
+            Regex r = new Regex(@"[0-9]+[a-zA-Z?]+[0-9]");
+            MatchCollection matches = r.Matches(str);
+            int flag = 0;
+            if (matches != null)
+            {
+                foreach (Match member in matches)
+                {
+                    string memberFirstAndLast = member.ToString();
+                    int sumOfMemberFirstAndLast = Convert.ToInt32(memberFirstAndLast.First().ToString()) + Convert.ToInt32(memberFirstAndLast.Last().ToString());
+
+                    if (sumOfMemberFirstAndLast == 10)
+                    {
+                        int countOfQuestionMark = memberFirstAndLast.Count(x => x == '?');
+                        if (3 <= countOfQuestionMark)
+                        {   
+                            flag=1;//if question mark count is more than 3,flag set=1 becouse this operation happen successful.
+                        }
+               
+                    }                 
+                    
+                }
+
+            }
+            if (flag==1)
+            {
+                return "True";
+            }
+            else
+            {
+                return "False";
+            }
+
+            
+        }
+    }
+}
+//acc?7??sss?3rr1??????5
